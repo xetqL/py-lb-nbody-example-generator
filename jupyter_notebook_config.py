f84162a5633f21e448587344d75eac4a55f20f59
anthony.boulmire@hesge.ch <anthony.boulmire@hesge.ch>
2017-10-20 17:11:47 +0200

    add jupyterApp config

---
+++ jupyter_notebook_config.py
@@ -0,0 +1,6 @@
+# Configuration file for jupyter-notebook.
+
+c.JupyterApp.password = 'sha1:ec48e66df773:1b5a5fe96ad4e3c47908fbb2ceb88d5910b796b4'
+c.JupyterApp.password_required=True
+c.JupyterApp.ip = '0.0.0.0'
+c.JupyterApp.port = 8888
